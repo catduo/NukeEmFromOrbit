5a5d8f36f91efae557b3ec6d0a8e8a824446b521
David Geisert <geisert@stanfordalumni.org>
2013-10-08 22:42:06 -0700

    art implemented plus a bunch of other upgrades

---
+++ Assets/Scripts/OtherPlanets.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OtherPlanets : MonoBehaviour {
+	
+	public float revolutionSpeed = 4F;
+	public float orbitSpeed = 0.5F;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}	
+	
+	void FixedUpdate(){
+		transform.Rotate(new Vector3(0, 0, revolutionSpeed));
+		transform.RotateAround(transform.parent.position, new Vector3(0,0,1), orbitSpeed);
+	}
+}
